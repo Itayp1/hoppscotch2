d5cc7e3e2c1045a95c2bfa284ff126b5cc71637a
Itay Peretz <Itayp1@users.noreply.github.com>
2026-06-14 22:57:43 +0300

    chore: ignore and untrack local package folders

--- packages/hoppscotch-js-sandbox/src/cage-modules/index.ts
+++
@@ -1,4 +0,0 @@
-export { defaultModules } from "./default"
-export { postRequestModule, preRequestModule } from "./scripting-modules"
-export { customCryptoModule } from "./crypto"
-export type { CustomCryptoModuleConfig } from "./crypto"
--- packages/hoppscotch-kernel/src/log/impl/web/index.ts
+++
@@ -1,5 +0,0 @@
-import { implementation as logV1 } from "./v/1"
-
-export const LOG_IMPLS = {
-  v1: logV1,
-} as const
